8dd4c71f226d644ecbd13b90a532fa1fb44e5e90
RITAADEDOLAPO <ritaadedolapo2@gmail.com>
2023-04-15 22:20:48 +0100

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import './components/users.css'
 import './components/search.css'
 import Users from './components/users';
@@ -5,10 +6,12 @@ import profileData from './components/profileData';
 import Search from './components/search';
 
 function App() {
+  const {search, setSearch} = useState('')
   const profileElement = profileData.map(data => {
         return (
+
           <Users 
-            
+            key = {data.id}
             picture = {data.picture}
             id = {data.id}
             title = {data.title}
